7bc2faa49a658a4855cea3cf8af9996303bb610d
Dilshodbek48 <xolmominovdilshodbek8888@gmail.com>
2025-02-08 14:10:46 +0500

    Add full site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const darkModeButton = document.querySelector('.dark-button');
+
+darkModeButton.addEventListener('click', function () {
+
+  document.body.classList.remove('dark-mode')
+})
+
+/* darkModeButton.addEventListener('click', function () {
+  document.body.classList.add('dark-mode')
+})
+ */
